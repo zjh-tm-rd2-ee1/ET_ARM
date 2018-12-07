2c5482b2593ba8d23ab448a5d2cc198d8ff59294
Ebenezer Scrooge <1641367382@qq.com>
2018-12-07 14:21:59 +0800

    Update test_et1.c

--- USER/test_et1.c
+++ USER/test_et1.c
@@ -8,46 +8,39 @@
 * Return: none
 * Call: external
 */
- void Test_ET1(void)
- {
- 	TOTAL_DIS_NUM = 5;	
- 	switch (DIS_NUM) 
-	{
-//		case (0): 
-//			FPGA_Info_Visible(INFO_VERSION | INFO_PROJECT_NO);
-//			FPGA_DisPattern(24, 127, 127, 127);	//dotcheck
-//			break;
-//		case (1):	
-//			FPGA_Info_Visible(INFO_NONE);
-		case (0):			
-			FPGA_Info_Visible(INFO_VERSION | INFO_PROJECT_NO);
-			ScanForward();	
- 			FPGA_DisPattern(0, 0, 0, 0);	//black
-			Delay_ms(ET_DLY_LOCK * 2700); 
-			break;
-		case (1): 	
-			FPGA_Info_Visible(INFO_RGBVAL);
-			ScanBackward();
- 			FPGA_DisPattern(0, 127, 127, 127);	//gray
-			Delay_ms(ET_DLY_LOCK * 3700); 
-			break;
-		case (2): 
+
+ // DJ code
+ 
+void Test_ET1(void){
+	TOTAL_DIS_NUM = 6;
+	switch (DIS_NUM){
+		case (0):
+//			FPGA_DisPicture(0);
+//			FPGA_Info_Set((uint8_t *)"HELLO WORLD");
+//			FPGA_Project_Set((uint8_t *)"I LOVE CAROL");
 			FPGA_Info_Visible(INFO_NONE);
-			ScanForward();
-			FPGA_DisPattern(0, 255, 255, 255);	//white
-		  Delay_ms(ET_DLY_LOCK * 700); 
+			FPGA_DisPattern(129, 0, 0x02, 0xC8);
 			break;
-		case (3): 
-			FPGA_DisPattern(127, 255, 255, 255);	//RGB
+		case (1):
+			FPGA_DisPattern(0, 255, 255, 255);
 			break;
-		case (4): 
+		case (2):
+			FPGA_DisPattern(0, 255, 0, 0);
+			break;
+		case (3):
+			FPGA_DisPattern(0, 0, 255, 0);
+			break;
+		case (4):
+			FPGA_DisPattern(0, 0, 0, 255);			
+			break;
+		case (5):
 			FPGA_DisPattern(0, 0, 0, 0);
-			KEY_SLEEPIN(); 
-		  Flag_Test_Current = RESET;				  //DO NOT test the current!
+			KEY_SLEEPIN();
+			Delay_ms(300);
 			break;
-		default: 
-			FPGA_DisPattern(0, 0, 0, 0); 
+		default:
+			FPGA_DisPattern(0, 0, 0, 0);
 			break;
 	}
-	Delay_ms(ET_DLY_LOCK * 300);
- }
+//	Delay_ms(ET_DLY_LOCK * 300);
+}
