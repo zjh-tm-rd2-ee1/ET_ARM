1e08caa6e66b7f6cfd6e8fcb60f9821eadbceca3
Ebenezer Scrooge <1641367382@qq.com>
2018-12-07 14:51:39 +0800

    Update test_ra.c
    USE ARM TO CONTROL LEDPWM

--- USER/test_ra.c
+++ USER/test_ra.c
@@ -8,7 +8,28 @@
 * Return: none
 * Call: external
 */
+u32 CURRENT2DC(u32 current){
+	//100%->20MA
+	u32 dc=current*100/20;
+	return dc;
+}
+void RA_LEDPWM(u32 current){
+	u32 duty_cycle=CURRENT2DC(current);
+	//u32 duty_cycle=50;
+	while(1){
+		u32 high_time = 20*duty_cycle/100;
+		GPIO_SetBits(TEST17_GPIO_PORT, TEST17_PIN);
+		Delay_us(high_time);
+		GPIO_ResetBits(TEST17_GPIO_PORT, TEST17_PIN);
+		Delay_us(20-high_time);
+	}
+}
+
 void Test_RA(void)
 {
-	;
+//	FPGA_DisPattern(83, 127, 127, 127);
+//	RA_LEDPWM(20);
+	//RA_LEDPWM(17);
+	//RA_LEDPWM(15);
+	//RA_LEDPWM(13);
 }
