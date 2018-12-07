22a7bd2cfa8f947ea0000c2df1849a10c529ef5c
Ebenezer Scrooge <1641367382@qq.com>
2018-12-07 14:26:19 +0800

    Update test_oqc1.c

--- USER/test_oqc1.c
+++ USER/test_oqc1.c
@@ -10,67 +10,104 @@
 */
  void Test_OQC1(void)
  {
- 	TOTAL_DIS_NUM = 11;
+ 	TOTAL_DIS_NUM = 20;
  	switch (DIS_NUM) 
 	{
 		case (0): 
-			FPGA_Info_Visible(INFO_VERSION | INFO_PROJECT_NO);
-			FPGA_DisPattern(81, 127, 0x77, 0x41);	//waku+black
-			break;
-		case (1):
-			FPGA_Info_Visible(INFO_NONE);
-		  ScanForward();
-		  FPGA_DisPattern(0, 0, 0, 0);	//black
-		  Delay_ms(ET_DLY_LOCK * 2700); 
-			break;
-		case (2): 
-		  LCD_PWM(0xFFF);
 			FPGA_Info_Visible(INFO_RGBVAL);
 			ScanBackward();
-			FPGA_DisPattern(0, 127, 127, 127);	//gray127
-			Delay_ms(ET_DLY_LOCK * 2700); 
+			FPGA_DisPattern(0, 127, 127, 127);
+			Delay_ms(3000);
+			break;
+		case (1):
+			FPGA_Info_Visible(INFO_VERSION|INFO_PROJECT_NO);
+		  ScanForward();
+		  FPGA_DisPattern(81, 0, 0x77, 0x41);	
+		  Delay_ms(300); 
+			break;
+		case (2): 
+			FPGA_Info_Visible(INFO_NONE);
+			FPGA_DisPattern(82, 0, 0, 0);
+			Delay_ms(3000); 
 			break;
 		case (3): 
 			LEDA_DIM();
-		  FPGA_Info_Visible(INFO_NONE);
-			ScanForward();
 			FPGA_DisPattern(0, 255, 255, 255);	//low current white
-		  SPEC_LEDA_MIN = SPEC_MIN_LEDA_DIM;
-		  SPEC_LEDA_MAX	= SPEC_MAX_LEDA_DIM;
+		  Delay_ms(1500); 
 			break;
 		case (4): 
-			LCD_PWM(0xFFF);
+			LCD_PWM(0x0FFF);
+			FPGA_DisPattern(94, 255, 0, 0);
+			Delay_ms(1500); 
+			break;
+		case (5): 
 			FPGA_DisPattern(0, 255, 255, 255);	//white
 			if (Pic_Load_Finish == SET || PIC_Load_BMP_ET((uint8_t *)"0.bmp") == ERROR)
 			{
 				Delay_ms(ET_DLY_LOCK * 2700); 
 			}
 			break;
-		case (5): 
-			FPGA_Info_Visible(INFO_NONE);
-			FPGA_DisPattern(90, 0, 0, 0);	//WRGB transtion
-			break;
 		case (6): 
+			FPGA_DisPattern(24, 127, 127, 127);
+			Delay_ms(1500);
+			break;
+		case (7): 
+			FPGA_DisPattern(138, 0, 0, 0);	//flicker
+			Delay_ms(300);
+			break;
+		case (8): 
+			FPGA_DisPattern(0, 255, 0, 0);	//dotcheck
+			Delay_ms(300);
+			break;
+		case (9): 
 			if (Pic_Load == ERROR)
 			{
 				FPGA_Info_Visible(INFO_STR);
 				FPGA_Info_Set((uint8_t *)"LOAD PIC FAIL.");
 			}
 			FPGA_DisPicture(0);	//picture
-			break;
-		case (7): 
-			FPGA_Info_Visible(INFO_NONE);
-			FPGA_DisPattern(22, 127, 127, 127);	//flicker
-			break;
-		case (8): 
-			FPGA_DisPattern(24, 127, 127, 127);	//dotcheck
-			break;
-		case (9): 
-			FPGA_DisPattern(1, 127, 127, 127);	//crosstalk
+			Delay_ms(300);
 			break;
 		case (10): 
+			FPGA_DisPattern(22, 127, 0, 0);
+			Delay_ms(300); 
+			break;
+		case (11): 
+			FPGA_DisPattern(1, 127, 0, 0);
+			Delay_ms(300); 
+			break;
+		case (12): 
+			FPGA_DisPattern(0, 0, 255, 0);
+			Delay_ms(300);  
+			break;
+		case (13): 
+			FPGA_DisPattern(0, 0, 0, 255);
+			Delay_ms(300);
+			break;
+		case (14): 
+			FPGA_DisPattern(17, 0, 0, 0); 	//bar
+			Delay_ms(300);
+			break;
+		case (15): 
+			FPGA_DisPattern(7, 127, 0, 0); 	//crosstalk
+			Delay_ms(300); 
+			break;
+		case (16): 
+			FPGA_DisPattern(133, 255, 0, 0); 	//HUAWEI TP
+			Delay_ms(300); 
+			break;
+		case (17): 
+			FPGA_DisPattern(136, 0, 0, 0); 	//HUAWEI S1
+			Delay_ms(300);
+			break;
+		case (18): 
+			FPGA_DisPattern(137, 0, 0, 0); 	//HUAWEI S1
+			Delay_ms(300);
+			break;
+		case (19): 
 			FPGA_DisPattern(0, 0, 0, 0);
-			KEY_SLEEPIN(); 
+			FPGA_Info_Visible(INFO_NONE);
+			KEY_SLEEPIN(); 	
 			break;
 		default: 
 			FPGA_DisPattern(0, 0, 0, 0); 
